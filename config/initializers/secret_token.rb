e5b36bf7baaedb5738f71c4cb585761e2c53889a
Liam Smith <liam.dj@gmail.com>
2013-02-27 15:28:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymathsportal::Application.config.secret_token = '66e4cd9aba20aa0f510717fddd46460812c299e50a5bcde346db1c596181277ba1646818a9f7cd0cad0b127f8c25e2072dd4d8efaf9d9ecc4318bf50a22a40d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mymathsportal::Application.config.session_store :cookie_store, key: '_mymathsportal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mymathsportal::Application.config.session_store :active_record_store
